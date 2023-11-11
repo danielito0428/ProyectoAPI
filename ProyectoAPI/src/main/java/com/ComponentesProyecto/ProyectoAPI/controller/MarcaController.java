af763d4eb20edf5ad24a718c704994b56e6b633f
danielito0428 <dgnavarro06@gmail.com>
2023-11-10 22:59:13 -0600

    Update MarcaController.java

--- ProyectoAPI/src/main/java/com/ComponentesProyecto/ProyectoAPI/controller/MarcaController.java
+++ ProyectoAPI/src/main/java/com/ComponentesProyecto/ProyectoAPI/controller/MarcaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URISyntaxException;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"https://reactcomponentes.azurewebsites.net", "http://localhost:3000/"})
 @RestController
 @RequestMapping("/api")
 public class MarcaController {
